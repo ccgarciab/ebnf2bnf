6444b39f21aa95f765de59d507f3fee03a858c43
Cristian Camilo Garcia Barrera <ccgarciab@unal.edu.co>
2022-06-20 09:23:44 -0500

    Simple main to parse an input file

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+from antlr4 import *
+from gen.grammarizerLexer import grammarizerLexer
+from gen.grammarizerParser import grammarizerParser
 from src.Visitor import Visitor
+
+
+def main():
+    input_file = FileStream("input.txt")
+    lexer = grammarizerLexer(input_file)
+    stream = CommonTokenStream(lexer)
+    parser = grammarizerParser(stream)
+    tree = parser.grammarFile()
+
+    visitor = Visitor()
+    grammar_map = visitor.visitGrammarFile(tree)
+
+
+if __name__ == '__main__':
+    main()
