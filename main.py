f65064033e85c00751bea47f546ff445aabbd10c
Cristian Camilo Garcia Barrera <ccgarciab@unal.edu.co>
2022-06-20 15:44:49 -0500

    Put recognizer access in http endpoint

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
-from antlr4 import CommonTokenStream, FileStream
+import logging
+
+from antlr4 import CommonTokenStream, InputStream
+from flask import Flask, request
 
 from gen.grammarizerLexer import grammarizerLexer
 from gen.grammarizerParser import grammarizerParser
 from src.Visitor import Visitor
 
+api = Flask(__name__)
 
-def main():
-    input_file = FileStream("input.txt")
-    lexer = grammarizerLexer(input_file)
+
+@api.route('/', methods=['GET'])
+def endpoint():
+    input_str = request.get_json()
+    logging.debug(f"input: {input_str}")
+    lexer = grammarizerLexer(InputStream(input_str))
     stream = CommonTokenStream(lexer)
     parser = grammarizerParser(stream)
     tree = parser.grammarFile()
 
     visitor = Visitor()
     grammar_map = visitor.visitGrammarFile(tree)
+
+    result_list = []
     for rule_name, nodes in grammar_map.items():
         for node in nodes:
             for rule in node.values():
-                print(f"{rule_name}: {rule or 'epsilon'};")
+                result_list.append(f"{rule_name}: {rule or 'epsilon'};")
+
+    return '\n'.join(result_list)
 
 
 if __name__ == '__main__':
-    main()
+    api.run()
