8c83a478a943d9c5f00b92ea4b9a2dba71d7fc98
Cristian Camilo García Barrera <ccgarciab@unal.edu.co>
2022-07-16 00:43:06 -0500

    feat: Implements syntax highlight
    The mechanism for syntax highlight consists of a textarea
    overlayed over a pre+code element. This arrangement allows
    interaction from the user with the convenient textarea while
    showing the user the highlighted text from the pre element.
    
    To allow this, the text area and it's contents are made transparent,
    and the dimensions and positioning of the elements are made identical.
    JS functions as event handlers allow keeping the text contents and
    scroll statuses of the elements synchronized.
    
    Syntax highlight styles and functionality provided by the Prism.js
    library: prismjs.com/
    
    Schema for editable highlighted text found at CSS-Tricks:
    css-tricks.com/creating-an-editable-textarea-that-supports-syntax-highlighted-code/

---
+++ assets/js/DomManipulation.js
@@ -0,0 +1,24 @@
+/*
+    Sycronize text content of a textarea that has received text and a
+    pre element that displays it
+*/
+function sync_text(origin_element, target_element) {
+    let text = origin_element.value;
+
+    // Extra space to ensure newline rendering
+    // Scroll to left to give an editor-like experience
+    if (text[text.length - 1] === "\n") {
+        text += " ";
+        origin_element.scrollLeft = 0;
+        target_element.scrollLeft = 0;
+    }
+    // Format text for display in pre + code element
+    target_element.innerHTML = text.replace(new RegExp("&", "g"), "&amp;").replace(new RegExp("<", "g"), "&lt;");
+    // Syntax highlight provided by Prism.js
+    Prism.highlightElement(target_element);
+}
+
+function sync_scroll(origin_element, target_element) {
+    target_element.scrollTop = origin_element.scrollTop;
+    target_element.scrollLeft = origin_element.scrollLeft;
+}
